909e8f37568cd40ea76c1b6f6b91a4d269d7ba24
Phong <p7157568@gmail.com>
2016-08-17 18:04:20 +0700

    feat - add layout for tablet

--- app/src/main/java/dhbk/android/testdesignandroid/MainActivity.java
+++ app/src/main/java/dhbk/android/testdesignandroid/MainActivity.java
@@ -1,12 +1,13 @@
 package dhbk.android.testdesignandroid;
 
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
+import android.support.v7.app.ActionBar;
 
 public class MainActivity extends BaseActivity {
-
+    /**
+     * Whether or not the activity is running on a device with a large screen
+     */
+    private boolean twoPaneMode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,10 +29,25 @@ public class MainActivity extends BaseActivity {
     }
 
     private void checkTabletOrMobile() {
-
+        if (isTwoPaneLayoutUsed()) {
+            twoPaneMode = true;
+            LogUtil.logD("TEST","TWO POANE TASDFES");
+            enableActiveItemState();
+        }
     }
 
+    /**
+     * Is the container present? If so, we are using the two-pane layout.
+     *
+     * @return true if the two pane layout is used.
+     */
+    private boolean isTwoPaneLayoutUsed() {
+        return findViewById(R.id.article_detail_container) != null;
+    }
 
+    /**
+     * fixme - change home button icon
+     */
     private void setupToolbar() {
         final ActionBar ab = getActionBarToolbar();
         ab.setHomeAsUpIndicator(R.drawable.ic_menu);
