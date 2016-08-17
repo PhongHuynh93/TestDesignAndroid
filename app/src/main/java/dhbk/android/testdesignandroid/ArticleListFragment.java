cb4171ac985d9349d04d0355d14be2a8bbc3cbf5
Phong <p7157568@gmail.com>
2016-08-17 19:07:15 +0700

    feat - add callback in ListFragment

--- app/src/main/java/dhbk/android/testdesignandroid/ArticleListFragment.java
+++ app/src/main/java/dhbk/android/testdesignandroid/ArticleListFragment.java
@@ -9,7 +9,12 @@ import android.support.v4.app.ListFragment;
  * A simple {@link Fragment} subclass.
  */
 public class ArticleListFragment extends ListFragment {
-
+    /**
+     * A callback interface. Called whenever a item has been selected.
+     */
+    public interface Callback {
+        void onItemSelected(String id);
+    }
 
     public ArticleListFragment() {
         // Required empty public constructor
--- app/src/main/java/dhbk/android/testdesignandroid/MainActivity.java
+++ app/src/main/java/dhbk/android/testdesignandroid/MainActivity.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.widget.ListView;
 
-public class MainActivity extends BaseActivity {
+public class MainActivity extends BaseActivity implements ArticleListFragment.Callback{
     /**
      * Whether or not the activity is running on a device with a large screen
      */
