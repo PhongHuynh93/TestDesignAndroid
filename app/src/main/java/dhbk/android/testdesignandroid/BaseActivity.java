385c38a849ee57be1c130000bd7036ec75f3a2a0
Phong <p7157568@gmail.com>
2016-08-17 19:20:13 +0700

    feat - declare nav

--- app/src/main/java/dhbk/android/testdesignandroid/BaseActivity.java
+++ app/src/main/java/dhbk/android/testdesignandroid/BaseActivity.java
@@ -1,14 +1,28 @@
 package dhbk.android.testdesignandroid;
 
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+import static dhbk.android.testdesignandroid.LogUtil.logD;
 
 /**
  * Created by huynhducthanhphong on 8/17/16.
  */
 public abstract class BaseActivity extends AppCompatActivity {
+    private static final int NAV_DRAWER_ITEM_INVALID = -1;
+    private static final String TAG = BaseActivity.class.getSimpleName();
     private Toolbar actionBarToolbar;
+    private DrawerLayout drawerLayout;
+    private NavigationView mUpDrawerSelectListener;
+    private NavigationView mSelectedItem;
 
     /**
      * Provides the action bar instance.
@@ -23,4 +37,124 @@ public abstract class BaseActivity extends AppCompatActivity {
         }
         return getSupportActionBar();
     }
+
+    /**
+     * run after onCreate of child
+     *
+     * @param savedInstanceState
+     */
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        setupNavDrawer();
+    }
+
+
+    /**
+     * Sets up the navigation drawer.
+     */
+    private void setupNavDrawer() {
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawerLayout == null) {
+            // current activity does not have a drawer.
+            return;
+        }
+
+        // TODO: 8/17/16 understand this method
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        if (navigationView != null) {
+            setupDrawerSelectListener(navigationView);
+            setSelectedItem(navigationView);
+        }
+
+        logD(TAG, "navigation drawer setup finished");
+    }
+
+
+
+    /**
+     * fixme - call the drawer, note that it can be null if current activity does not have a drawer.(because it's a base activity)
+     */
+    public void openDrawer() {
+        if(drawerLayout == null)
+            return;
+        drawerLayout.openDrawer(GravityCompat.START);
+    }
+
+    /**
+     * Creates the item click listener.
+     * @param navigationView the navigation view
+     */
+    private void setupDrawerSelectListener(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        drawerLayout.closeDrawers();
+                        onNavigationItemClicked(menuItem.getItemId());
+                        return true;
+                    }
+                });
+    }
+
+    /**
+     * Handles the navigation item click.fixme not understand this method, why is close and return, but not go to another activity
+     * @param itemId the clicked item
+     */
+    private void onNavigationItemClicked(final int itemId) {
+        if(itemId == getSelfNavDrawerItem()) {
+            // Already selected
+            closeDrawer();
+            return;
+        }
+
+        goToNavDrawerItem(itemId);
+    }
+
+
+    /**
+     * Returns the navigation drawer item that corresponds to this Activity. Subclasses
+     * have to override this method.
+     */
+    protected int getSelfNavDrawerItem() {
+        return NAV_DRAWER_ITEM_INVALID;
+    }
+
+
+    protected void closeDrawer() {
+        if(drawerLayout == null)
+            return;
+
+        drawerLayout.closeDrawer(GravityCompat.START);
+    }
+
+    /**
+     * Handles the navigation item click and starts the corresponding activity.
+     * @param item the selected navigation item
+     */
+    private void goToNavDrawerItem(int item) {
+        switch (item) {
+            case R.id.nav_quotes:
+                startActivity(new Intent(this, ListActivity.class));
+                finish();
+                break;
+            case R.id.nav_samples:
+                startActivity(new Intent(this, ViewSamplesActivity.class));
+                break;
+            case R.id.nav_settings:
+                startActivity(new Intent(this, SettingsActivity.class));
+                break;
+        }
+    }
+
+    /**
+     * Updated the checked item in the navigation drawer
+     * @param navigationView the navigation view
+     */
+    private void setSelectedItem(NavigationView navigationView) {
+        // Which navigation item should be selected?
+        int selectedItem = getSelfNavDrawerItem(); // subclass has to override this method
+        navigationView.setCheckedItem(selectedItem);
+    }
+
 }
---
+++ app/src/main/java/dhbk/android/testdesignandroid/ViewSamplesActivity.java
@@ -0,0 +1,13 @@
+package dhbk.android.testdesignandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ViewSamplesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_samples);
+    }
+}
