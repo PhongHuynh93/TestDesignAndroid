620750c2d9cfc69db847405103c6aafecedc5123
Phong <p7157568@gmail.com>
2016-08-17 19:04:14 +0700

    feat - fix crash

--- app/src/main/java/dhbk/android/testdesignandroid/ArticleListFragment.java
+++ app/src/main/java/dhbk/android/testdesignandroid/ArticleListFragment.java
@@ -1,12 +1,8 @@
 package dhbk.android.testdesignandroid;
 
 
-import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.ListFragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
 
 /**
@@ -17,14 +13,5 @@ public class ArticleListFragment extends ListFragment {
 
     public ArticleListFragment() {
         // Required empty public constructor
-    }
-
-
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_article_list, container, false);
     }
 }
